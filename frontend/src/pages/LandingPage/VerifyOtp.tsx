6fca28969b62f0a050d9719dd56ca142ed0758cc
Arpith Paliwal <arpithpaliwal@gmail.com>
2025-12-29 21:32:40 +0530

    feat: add OTP input component and integrate it into the VerifyOtp page

--- frontend/src/pages/LandingPage/VerifyOtp.tsx
+++ frontend/src/pages/LandingPage/VerifyOtp.tsx
@@ -1,10 +1,31 @@
+import OtpInput from "../../components/Otp/OtpInput";
 
 function VerifyOtp() {
+  const handleOtpComplete = (otp: string) => {
+    console.log("submitted:", otp);
+
+    
+  };
+
   return (
-    <div>
-      
+    <div className="relative flex justify-center items-center bg-background overflow-hidden p-4 min-h-screen">
+      <div className="w-full max-w-md bg-accent border border-[#0096c7]/30 p-7 rounded-2xl shadow-lg flex justify-center items-center flex-col">
+        <div className="md:h-[20vh] md:w-[20vw]">
+          <img
+            src="name_light-theme.svg"
+            className="block h-full w-full object-contain"
+          />
+          <img
+            src="name_dark-theme.svg"
+            className="hidden h-full w-full object-contain"
+          />
+        </div>
+
+        <OtpInput length={6} onComplete={handleOtpComplete} />
+        
+      </div>
     </div>
-  )
+  );
 }
 
-export default VerifyOtp
+export default VerifyOtp;
--- frontend/src/routes/routes.tsx
+++ frontend/src/routes/routes.tsx
@@ -2,12 +2,14 @@ import { createBrowserRouter, createRoutesFromElements, Route } from "react-rout
 import LandingPage from "../pages/LandingPage/LandingPage";
 import SignupPage from "../pages/LandingPage/SignupPage";
 import LoginPage from "../pages/LandingPage/LoginPage";
+import VerifyOtp from "../pages/LandingPage/VerifyOtp";
 const router = createBrowserRouter(
   createRoutesFromElements(
     <>
       <Route path="/" element={<LandingPage />} />
       <Route path="/signup" element={<SignupPage />} />
       <Route path="/login" element={<LoginPage />} />
+      <Route path="/verifyotp" element={<VerifyOtp />} />
     </>
   )
 );
